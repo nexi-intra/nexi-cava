6f33238e147edae4f445aa68c9442fa47d68a134
Niels Gregers Johansen <niels@timeboxer.dk>
2024-03-20 11:57:53 +0100

    more

--- .koksmat/web/koksmat/msal/config.ts
+++ .koksmat/web/koksmat/msal/config.ts
@@ -1,53 +1,56 @@
 import { Configuration, PopupRequest } from "@azure/msal-browser";
 // Config object to be passed to Msal on creation
 import { LogLevel } from "@azure/msal-browser";
-export const msalConfig : Configuration  = {
+export const msalConfig: Configuration = {
+  auth: {
+    clientId:
+      process.env.NEXT_PUBLIC_MSAL_CLIENT_ID ??
+      "81ce343c-4ac6-4e52-9777-7534387545ba",
 
-    auth: {
-        clientId: "81ce343c-4ac6-4e52-9777-7534387545ba",
-        
-        authority: "https://login.microsoftonline.com/79dc228f-c8f2-4016-8bf0-b990b6c72e98",
-        redirectUri: "/",
-        postLogoutRedirectUri: "/"
+    authority:
+      process.env.NEXT_PUBLIC_MSAL_AUTHORITY ??
+      "https://login.microsoftonline.com/79dc228f-c8f2-4016-8bf0-b990b6c72e98",
+    redirectUri: "/",
+    postLogoutRedirectUri: "/",
+  },
+  cache: {
+    cacheLocation: "localStorage", // This configures where your cache will be stored
+    storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback: (level, message, containsPii) => {
+        if (containsPii) {
+          return;
+        }
+        switch (level) {
+          case LogLevel.Error:
+            console.error(message);
+            return;
+          case LogLevel.Info:
+            console.info(message);
+            return;
+          case LogLevel.Verbose:
+            console.debug(message);
+            return;
+          case LogLevel.Warning:
+            console.warn(message);
+            return;
+          default:
+            return;
+        }
+      },
     },
-    cache: {
-        cacheLocation: "localStorage", // This configures where your cache will be stored
-        storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
-    },
-    system: {
-        loggerOptions: {
-            loggerCallback: (level, message, containsPii) => {
-                if (containsPii) {
-                    return;
-                }
-                switch (level) {
-                    case LogLevel.Error:
-                        console.error(message);
-                        return;
-                    case LogLevel.Info:
-                        console.info(message);
-                        return;
-                    case LogLevel.Verbose:
-                        console.debug(message);
-                        return;
-                    case LogLevel.Warning:
-                        console.warn(message);
-                        return;
-                    default:
-                        return;
-                }
-            }
-        },
-        allowNativeBroker: false, // Disables WAM Broker
-    }
+    allowNativeBroker: false, // Disables WAM Broker
+  },
 };
 
 // Add here scopes for id token to be used at MS Identity Platform endpoints.
 export const loginRequest = {
-    scopes: ["User.Read"]
+  scopes: ["User.Read"],
 };
 
 // Add here the endpoints for MS Graph API services you would like to use.
 export const graphConfig = {
-    graphMeEndpoint: "https://graph.microsoft.com/v1.0/me"
+  graphMeEndpoint: "https://graph.microsoft.com/v1.0/me",
 };
