ff68bf075b02fcf20fe7776a06e3f366681de9cb
Niels Gregers Johansen <niels@timeboxer.dk>
2024-06-07 11:37:16 +0200

    fix nats package issue

--- .koksmat/web/app/cava/components/nats.tsx
+++ .koksmat/web/app/cava/components/nats.tsx
@@ -1,21 +1,14 @@
 "use client";
 
 import { useEffect, useMemo, useRef, useState } from "react";
-import { connect, NatsConnection, StringCodec } from "nats.ws";
-
-
 
 export default function ShowNatsLog(props: { subject: string }) {
-  const logEntries2 = useRef<string[]>([])
-
+  const logEntries2 = useRef<string[]>([]);
 
   return (
     <div className="border">
-
       {logEntries2.current.map((entry, i) => (
-      <div key={i}>
-        {entry}</div>
-    
+        <div key={i}>{entry}</div>
       ))}
     </div>
   );
--- .koksmat/web/koksmat/server/runKoksmatProcess.ts
+++ .koksmat/web/koksmat/server/runKoksmatProcess.ts
@@ -1,59 +1,32 @@
 import { Result } from "@/koksmat/httphelper";
 import { AnyARecord } from "dns";
-import { connect, Msg, Payload, ServiceError, StringCodec } from "nats.ws";
 
 export interface INatsConfig {
-  servers : string[]
+  servers: string[];
 }
 
-export const runKoksmatProcess = (command: string, args: string[], timeout: number, channel: string, natsConfig : INatsConfig): Promise<Result<string>> => {
+export const runKoksmatProcess = (
+  command: string,
+  args: string[],
+  timeout: number,
+  channel: string,
+  natsConfig: INatsConfig
+): Promise<Result<string>> => {
   return new Promise(async (resolve, reject) => {
-
     let result: Result<any> = {
       hasError: false,
       errorMessage: "",
       data: "",
     };
     try {
-
-      const {servers} = natsConfig
-
-      const nc = await connect({
-        servers
-      })
-      
-
-      const sc = StringCodec();
-      const payload: Payload = JSON.stringify({ command, args, timeout, channel:"log."+channel });
-      
-       console.log("runKoksmatProcess", command, args, timeout, channel, payload)
-    
-      const msg = await nc.request(command, payload, { timeout: timeout * 1000 });
-      result.data = sc.decode(msg.data);
-      // if (!ServiceError.isServiceError(msg)) {
-      //   result.data = sc.decode(msg.data);
-      //   result.hasError = false;
-      // } else {
-    
-      //   result.hasError = true;
-      //   console.log("error", ServiceError.toServiceError(msg));
-      //   result.errorMessage =ServiceError.toServiceError(msg)?.message
-      // }
-      
-   
-
-
-    } catch (error ) {
+    } catch (error) {
       const e = error as Error;
-      
+
       result.hasError = true;
       console.log("error", e.message ?? error);
       result.errorMessage = "NATS error";
-
     }
 
-
     resolve(result);
-
   });
 };
